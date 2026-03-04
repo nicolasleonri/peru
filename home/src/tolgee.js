03d8be381c4d398b8ac03192f21c52fd18e40d44
seffs <46768740+seffs@users.noreply.github.com>
2026-03-04 15:33:09 +0100

    feat: parse fallback i18n files

--- home/src/tolgee.js
+++ home/src/tolgee.js
@@ -1,13 +1,29 @@
 import { Tolgee, DevTools, FormatSimple, BackendFetch } from "@tolgee/react";
 
+const getStaticData = {
+    es: () => import("../i18n/es.json").then(m => m.default),
+};
+
 export const tolgee = Tolgee()
   .use(DevTools())
   .use(FormatSimple())
-  .use(BackendFetch({ prefix: import.meta.env.VITE_I18N_URL }))
+  .use(BackendFetch({
+      prefix: import.meta.env.VITE_I18N_URL,
+      fallbackOnFail: true,
+      timeout: 5000,
+      getData: async response => {
+          if (!response.ok) {
+              console.warn('Tolgee: Network response was not ok, using static translations');
+              throw new Error('Network response was not ok');
+          }
+          return response.json();
+      }
+  }))
   .init({
     apiUrl: import.meta.env.VITE_TOLGEE_API_URL,
     apiKey: import.meta.env.VITE_TOLGEE_API_KEY,
     defaultLanguage: 'es',
     availableLanguages: ['es'],
     fallbackLanguage: 'es',
+    staticData: getStaticData
   });
