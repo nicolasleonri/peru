9f0299f4b879faf452d878b1d681de2de913fff5
seffs <46768740+seffs@users.noreply.github.com>
2026-03-13 21:35:33 +0100

    feat: aymara hero title

--- home/src/tolgee.js
+++ home/src/tolgee.js
@@ -3,6 +3,7 @@ import { Tolgee, DevTools, FormatSimple, BackendFetch } from "@tolgee/react";
 const getStaticData = {
     es: () => import("../i18n/es.json").then(m => m.default),
     qu: () => import("../i18n/qu.json").then(m => m.default),
+    ay: () => import("../i18n/ay.json").then(m => m.default),
 };
 
 export const tolgee = Tolgee()
@@ -24,7 +25,7 @@ export const tolgee = Tolgee()
     apiUrl: import.meta.env.VITE_TOLGEE_API_URL,
     apiKey: import.meta.env.VITE_TOLGEE_API_KEY,
     defaultLanguage: 'es',
-    availableLanguages: ['es', 'qu'],
+    availableLanguages: ['es', 'qu', 'ay'],
     fallbackLanguage: 'es',
     staticData: getStaticData
   });
